8cc434543f4b80d2d773bc58e8dbf0515eb82eb7
David Webber <hello@david-webber.co.uk>
2020-04-27 23:19:06 +0100

    animate drips

--- assets/index.js
+++ assets/index.js
@@ -5,13 +5,14 @@ const dtx = dripCanvas.getContext("2d");
 
 let isDrawing = false;
 let count = 0;
-const minDrip = 2;
-const maxDrip = 300;
-const dripTolerance = 2;
-let dripStarted = false;
 
-const height = window.innerHeight/2;
-const width = window.innerWidth/2;
+const minDrip = 1;
+const maxDrip = 400;
+
+let dripAnimationController;
+
+const height = window.innerHeight;
+const width = window.innerWidth;
 
 resizeCanvas();
 
@@ -21,7 +22,7 @@ img.onload = function () {
 	ctx.drawImage(img, 0, 0, (width- 10),(height-10));
 };
 
-img.src = './assets/Hello_my_name_is_sticker.svg';
+img.src = '/assets/Hello_my_name_is_sticker.svg';
 
 
 
@@ -33,8 +34,9 @@ document.addEventListener('mouseup', stopDrawing);
 
 const penOptions = {
 	colour: '#000000',
-	width: 30,
-	tip: 'bevel' //round,bevel,miter
+	width: 50,
+	dripTolerance: 2,
+	tip: 'round' //round,bevel,miter
 }
 
 setInterval(function () {
@@ -45,17 +47,16 @@ setInterval(function () {
 
 function startDrawing(e) {
 	isDrawing = true;
-
+	// ctx.moveTo(e.clientX,e.clientY)
 	ctx.beginPath();
 	dtx.beginPath();
 	count = 1;
 	draw(e)
-
 }
 
 function stopDrawing() {
 	isDrawing = false;
-
+	ctx.closePath();
 	count = 1;
 }
 
@@ -65,104 +66,53 @@ function draw({
 	clientY: y
 }) {
 	if (!isDrawing) return;
-	// console.log('draw',x,y);
-	// console.log('is drawing', isDrawing);
+
 	const {
 		colour,
 		width,
-		tip
+		tip, dripTolerance
 	} = penOptions;
 	ctx.lineWidth = width;
 	ctx.strokeStyle = colour;
-	ctx.fillStyle = colour;
 	ctx.lineJoin = tip;
-
-
-	// ctx.moveTo(x,y);
-	//	count++;
 	ctx.lineTo(x, y);
 	ctx.stroke();
-	// ctx.beginPath();
-	// ctx.moveTo(x,y)
-	// ctx.closePath();
-	if (count % dripTolerance == 0) {
-		drip(x, y);
-		// if(!dripStarted){
-		// 	startDrip(x, y)
-		// }
+	//ctx.closePath();
+	if (count % dripTolerance == 0 || dripTolerance == 0) {
+		const dripLength = Math.floor(Math.random() * (maxDrip - minDrip + 1) + minDrip);
+		startDrip(x,y,(y+dripLength),y);
 	}
 
 }
 
 
-let dripEnd = 0;
-
-function startDrip(x, y) {
-
-	const {
-		colour,
-		width,
-		tip
-	} = penOptions;
-	dtx.strokeStyle = colour;
-	dtx.fillStyle = colour;
-	dtx.lineJoin = tip;
-	dtx.beginPath();
-	dtx.linecap = "round"
 
 
-	let minDripWidth = 0.5;
-	let maxDripWidth = (width / 8 < 1) ? 1 : width / 8;
-	dtx.lineWidth = Math.floor(Math.random() * (maxDripWidth - minDripWidth + 1) + minDripWidth);
+// function drip(x, y) {
+// 	const {
+// 		colour,
+// 		width,
+// 		tip
+// 	} = penOptions;
+// 	dtx.strokeStyle = colour;
+// 	dtx.fillStyle = colour;
+// 	dtx.lineJoin = tip;
+// 	dtx.beginPath();
+// 	dtx.linecap = "round"
 
 
-	dripStarted = true;
-	dripEnd = y + maxDrip;
-	drawDrip(x, y);
-}
-
-function drawDrip(x, dripPosition) {
-
-	dtx.moveTo(x, dripPosition);
-	dtx.lineTo(x, dripPosition + 1);
-	dtx.stroke();
-	dtx.closePath();
+// 	let minDripWidth = 0.5;
+// 	let maxDripWidth = (width / 8 < 1) ? 1 : width / 8;
+// 	dtx.lineWidth = Math.floor(Math.random() * (maxDripWidth - minDripWidth + 1) + minDripWidth);
 
 
-	if (dripPosition < dripEnd) {
-		drawDrip(x, dripPosition + 1);
-	} else {
-		dripStarted = false;
-	}
+// 	const dripLength = Math.floor(Math.random() * (maxDrip - minDrip + 1) + minDrip);
 
-}
-
-
-function drip(x, y) {
-	const {
-		colour,
-		width,
-		tip
-	} = penOptions;
-	dtx.strokeStyle = colour;
-	dtx.fillStyle = colour;
-	dtx.lineJoin = tip;
-	dtx.beginPath();
-	dtx.linecap = "round"
-
-
-	let minDripWidth = 0.5;
-	let maxDripWidth = (width / 8 < 1) ? 1 : width / 8;
-	dtx.lineWidth = Math.floor(Math.random() * (maxDripWidth - minDripWidth + 1) + minDripWidth);
-
-
-	const dripLength = Math.floor(Math.random() * (maxDrip - minDrip + 1) + minDrip);
-
-	dtx.moveTo(x, y);
-	dtx.lineTo(x, y + dripLength);
-	dtx.stroke();
-	dtx.closePath();
-}
+// 	dtx.moveTo(x, y);
+// 	dtx.lineTo(x, y + dripLength);
+// 	dtx.stroke();
+// 	dtx.closePath();
+// }
 
 
 
@@ -195,16 +145,13 @@ function resizeCanvas() {
 
 
 
-var anim = 0;
-var animation;
 
-function repeatOften() {
-	console.log('repeat');
-	if(anim > 100){
+function drawDrip(x,start,end,current) {
+
+	if(current > end){
 		stopAnimation();
 		return;
 	}
-	anim++;
 
 	const {
 		colour,
@@ -214,156 +161,34 @@ function repeatOften() {
 	dtx.strokeStyle = colour;
 	dtx.fillStyle = colour;
 	dtx.lineJoin = tip;
-	dtx.beginPath();
+	//dtx.beginPath();
 	dtx.linecap = "round"
-	lineWidth = 2;
+	lineWidth = 4;
 
-	dtx.moveTo(20, anim+40);
-	dtx.lineTo((20+lineWidth), anim+40);
+
+	dtx.moveTo(x,current);
+
+	current+=10;
+
+//	dtx.moveTo(x, start);
+	dtx.lineTo(x, current);
 	dtx.stroke();
 	dtx.closePath();
 
 
-
-
-
-
-	animation = requestAnimationFrame(repeatOften);
-
-
-
-
+	dripAnimationController = requestAnimationFrame(function(){
+		drawDrip(x,start,end,current)
+	});
 }
 
-function startAnimation(){
-	animation = requestAnimationFrame(repeatOften);
+function startDrip(x,start,end,current){
+	dripAnimationController = requestAnimationFrame(function(){
+		drawDrip(x,start,end,current)
+	})
 }
 
 
 function stopAnimation(){
-  cancelAnimationFrame(animation);
+  cancelAnimationFrame(dripAnimationController);
 }
 
-
-
-startAnimation();
-
-
-
-
-
-
-
-
-
-// (function () {
-// 	var lastTime = 0;
-// 	var vendors = ['ms', 'moz', 'webkit', 'o'];
-// 	for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-// 			window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
-// 			window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
-// 	}
-
-// 	if (!window.requestAnimationFrame) window.requestAnimationFrame = function (callback, element) {
-// 			var currTime = new Date().getTime();
-// 			var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-// 			var id = window.setTimeout(function () {
-// 					callback(currTime + timeToCall);
-// 			},
-// 			timeToCall);
-// 			lastTime = currTime + timeToCall;
-// 			return id;
-// 	};
-
-// 	if (!window.cancelAnimationFrame) window.cancelAnimationFrame = function (id) {
-// 			clearTimeout(id);
-// 	};
-// }());
-
-
-
-
-
-// // variable to hold how many frames have elapsed in the animation
-// var t = 1;
-
-// // define the path to plot
-// var vertices = [];
-// vertices.push({
-// 	x: 0,
-// 	y: 0
-// });
-// vertices.push({
-// 	x: 300,
-// 	y: 100
-// });
-// vertices.push({
-// 	x: 80,
-// 	y: 200
-// });
-// vertices.push({
-// 	x: 10,
-// 	y: 100
-// });
-// vertices.push({
-// 	x: 0,
-// 	y: 0
-// });
-
-// // draw the complete line
-// ctx.lineWidth = 1;
-// // tell canvas you are beginning a new path
-// ctx.beginPath();
-// // draw the path with moveTo and multiple lineTo's
-// ctx.moveTo(0, 0);
-// ctx.lineTo(300, 100);
-// ctx.lineTo(80, 200);
-// ctx.lineTo(10, 100);
-// ctx.lineTo(0, 0);
-// // stroke the path
-// ctx.stroke();
-
-
-// // set some style
-// ctx.lineWidth = 5;
-// ctx.strokeStyle = "blue";
-// // calculate incremental points along the path
-// var points = calcWaypoints(vertices);
-// // extend the line from start to finish with animation
-// animate(points);
-
-
-// // calc waypoints traveling along vertices
-// function calcWaypoints(vertices) {
-// 	var waypoints = [];
-// 	for (var i = 1; i < vertices.length; i++) {
-// 			var pt0 = vertices[i - 1];
-// 			var pt1 = vertices[i];
-// 			var dx = pt1.x - pt0.x;
-// 			var dy = pt1.y - pt0.y;
-// 			for (var j = 0; j < 100; j++) {
-// 					var x = pt0.x + dx * j / 100;
-// 					var y = pt0.y + dy * j / 100;
-// 					waypoints.push({
-// 							x: x,
-// 							y: y
-// 					});
-// 			}
-// 	}
-// 	return (waypoints);
-// }
-
-
-// function animate() {
-// 	if (t < points.length - 1) {
-// 			requestAnimationFrame(animate);
-// 	}
-// 	// draw a line segment from the last waypoint
-// 	// to the current waypoint
-// 	ctx.beginPath();
-// 	ctx.moveTo(points[t - 1].x, points[t - 1].y);
-// 	ctx.lineTo(points[t].x, points[t].y);
-// 	ctx.stroke();
-// 	// increment "t" to get the next waypoint
-// 	t++;
-// }
